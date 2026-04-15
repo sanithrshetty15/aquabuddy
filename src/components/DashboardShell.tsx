7008fc5a92334aaa9a4d66f2392481cdc1edc604
sanithrshetty15 <sanithrshetty15@gmail.com>
2026-04-15 16:42:45 +0530

    Update DashboardShell

--- src/components/DashboardShell.tsx
+++ src/components/DashboardShell.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { motion, AnimatePresence } from "framer-motion";
